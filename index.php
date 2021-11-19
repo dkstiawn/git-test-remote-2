2bd1aff9cc55f4932f9484535efea2d3299b68d8
Dika Setiawan <dikaleovarista@gmail.com>
2021-11-19 20:37:35 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Coba Git </title>
+	<style type="text/css">
+		body {
+				font-family: Times new roman;
+	}
+	</style>
+</head>
+<body>
+	<h1> coba git gaes </h1>
+	<p> aku suka ngoding </p>
+<script type="text/javascript" src="script.js"></script>	
+</body>
+</html>
